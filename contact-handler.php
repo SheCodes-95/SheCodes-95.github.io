3c1e259627b4295b4504ed9fb5e50b7bd156edc0
SheCodes-95 <54573209+SheCodes-95@users.noreply.github.com>
2020-01-12 23:44:59 +0200

    3rd commit
    Changed both skill and php webpages

--- contact-handler.php
+++ contact-handler.php
@@ -1,20 +1,19 @@
 <?php
+ if(isset($_POST['submit'])){
  $name= $_POST['name'];
  $surname=$_POST['surname'];
- $visitor_email=$_POST['email'];
+ $mailFrom=$_POST['mail'];
  $message=$_POST['message'];
- $email_from='tumelophela@outlook.com';
- $email_subject="New contact from submission";
- $email_body="User Name: $name.\n".
-             "User Surname: $surname.\n".
-             "User email: $email.\n".
-             "User message: $message.\n";
 
-$to="tumelophela@gmail.com";
-$headers="From:  $email_from \r\n";
-$headers .="Reply-To: $visitor_email \r\n";
 
-mail($to, $email_subject,$email_body,$headers);
+$mailTo="tumelophela@outlook.com";
+$headers="From: ".$mailFrom;
+$txt="You have received an e-mail from ".$name.".\n\n".$message;
+
+
+mail($mailTo, $name, $surname, $txt, $headers );
 header("Location: contact.html") ;
 
+
+}
 ?>
